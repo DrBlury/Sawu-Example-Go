b2c7d41b1a6618aa236538faffc41e69ca694077
Florian Grote <info@grote-florian.de>
2021-04-19 14:40:13 +0200

    UrlPath encoding to support all kind of data

--- src/Main.go
+++ src/Main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 	"os"
 	"sawu-example-go/config"
 	"sawu-example-go/kafka"
@@ -13,35 +15,45 @@ import (
 var app *fiber.App
 
 func main() {
-	var fiberConfig = fiber.Config{}
-	fiberConfig.DisableStartupMessage = false
-	app = fiber.New(fiberConfig)
-	config.GetDefaults()
-	addRestController()
-	steps.LoadStepFunctions()
-	go kafka.DoKafkaConsumerStuff()
-	kafka.CreateProducer()
+    var fiberConfig = fiber.Config{}
+    fiberConfig.DisableStartupMessage = false
+    app = fiber.New(fiberConfig)
+    config.GetDefaults()
+    addRestController()
+    steps.LoadStepFunctions()
+    go kafka.DoKafkaConsumerStuff()
+    kafka.CreateProducer()
 
-	//Set default port if not set
-	port, isPresent := os.LookupEnv("fiber_port")
-	if isPresent == false {
-		port = config.Defaults.Port
-	}
-	//kafka.CreateProducer()
-	app.Listen(":" + port)
+    //Set default port if not set
+    port, isPresent := os.LookupEnv("fiber_port")
+    if !isPresent {
+        port = config.Defaults.Port
+    }
+    //kafka.CreateProducer()
+    app.Listen(":" + port)
 }
 
 // AddRestController creates the controller for all sorts of things
 func addRestController() {
-	app.Get("/start/:data", func(c *fiber.Ctx) error {
-		// Generate new Event
-		nextStepEvent := steps.GenerateNewNextStepEventForNewProcess("StepOne")
-		nextStepEvent.Data = c.Params("data")
-		// Send Event to Kafka
-		kafka.SendNextStepEvent(nextStepEvent)
+    // TODO move to POST and body .. if time
+    app.Get("/start/:data", func(c *fiber.Ctx) error {
+        // Generate new Event
+        nextStepEvent := steps.GenerateNewNextStepEventForNewProcess("StepOne")
+        value, err := url.PathUnescape(c.Params("data"))
+        if err != nil {
+            log.Fatal(err)
+            return c.Status(400).JSON(&fiber.Map{
+                "error": fmt.Sprintf("Failed to decode query param 'data': %s", c.Params("data")),
+            })
+        }
 
-		return c.Status(202).JSON(&fiber.Map{
-			"message": fmt.Sprintf("Accepted. Started processing with params: %s.", c.Params("data")),
-		})
-	})
+        nextStepEvent.Data = value
+
+        // Send Event to Kafka
+        kafka.SendNextStepEvent(nextStepEvent)
+
+        return c.Status(202).JSON(&fiber.Map{
+            "message": fmt.Sprintf("Accepted. Started processing with params: %s.", value),
+        })
+    })
 }
